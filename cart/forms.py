78da754bf00121985801832765afce9e161dc6b2
Kavin2610 <kavin2610@gmail.com>
2023-10-05 23:40:34 +0530

    added cart functionality

---
+++ cart/forms.py
@@ -0,0 +1,15 @@
+from .models import checkoutdetails
+from django import forms
+class CheckoutForm(forms.ModelForm):
+    class Meta:
+        model = checkoutdetails
+        fields = ['firstName','lastName', 'address','postalcode', 'city','phoneNumber']
+
+    firstName = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'enter your firstname'}))
+    lastName = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'enter your lastname'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'enter your address'}))
+    postalcode = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'enter your postalcode'}))
+    city = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'enter your city'}))
+    phoneNumber = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'enter your phoneNumber'}))
+
+    
